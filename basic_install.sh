e577eb6ffdfb4613bed94bec0f45730e5f143da2
broadsun <onlyforsgb@163.com>
2018-04-27 21:52:09 +0800

    add basic_install.sh

---
+++ basic_install.sh
@@ -0,0 +1,9 @@
+sudo yum -y  install gcc gcc-c++
+sudo yum -y install curl-devel expat-devel gettext-devel openssl-devel zlib-devel
+sudo yum -y install git-core
+sudo yum -y install lrzsz
+sudo yum -y install scons
+sudo yum -y install libstdc++*
+sudo yum -y install tmux
+sudo yum -y install autojump && echo ". /usr/share/autojump/autojump.bash" >> ~/.bashrc && source ~/.bashrc
+sudo yum -y install nload 
